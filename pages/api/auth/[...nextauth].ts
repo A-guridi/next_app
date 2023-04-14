f119d64407179f6da42af099f168dc28c65ee36e
ge93sut <ge93sut@mytum.de>
2023-04-14 18:40:51 +0200

    Initial commit from create-liveblocks-app

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+import { getUser } from "../../../lib/server";
+import { User } from "../../../types";
+
+// Your NextAuth secret (generate a new one for production)
+// More info: https://next-auth.js.org/configuration/options#secret
+export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
+
+export const authOptions = {
+  secret: NEXTAUTH_SECRET,
+  callbacks: {
+    // Get extra user info from your database to pass to front-end
+    // For front end, update next-auth.d.ts with session type
+    async session({ session }: { session: any }) {
+      const userInfo: User | null = await getUser(session.user.email);
+
+      if (!userInfo) {
+        throw new Error("User not found");
+      }
+
+      session.user.info = userInfo;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/signin",
+  },
+
+  // Configure one or more authentication providers
+  // More info: https://next-auth.js.org/providers/
+  providers: [
+    // Use Auth0 authentication
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID as string,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+      issuer: process.env.AUTH0_ISSUER_BASE_URL,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
